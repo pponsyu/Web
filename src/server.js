97d29bb2ab828a67c528925373ec1f9e9eec9402
Yuri Pons <Yuri.PONS@etu.uca.fr>
2018-11-30 09:38:19 +0100

    Changement port 8080

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const app = express()
-const models = require('./models/index');
+//const express = require('express')
+//const bodyParser = require('body-parser');
+//const morgan = require('morgan');
+//const app = express()
+//const models = require('./models/index');
 
 console.log('Japprecie les fruits aux sirops')
 
@@ -14,31 +14,39 @@ app.use(bodyParser.json())
 app.use(morgan('short'))
 
 // Get all the users defined
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   models.User.findAll()
     .then((users) => {
       res.json(users)
     })
+})*/
+
+app.get('/', function (req, res) {
+
+    console.log("Hello World")
 })
 
 // Add a new user to the database
-app.post('/', function(req, res) {
+/*app.post('/', function(req, res) {
   models.User.create({
     username: req.body.username
   })
     .then(() => {
       res.send('User added !')
     })
-})
+})*/
+
+//process.env.ENABLE_METRICS
+
 
 // Synchronize models
-models.sequelize.sync().then(function() {
+//models.sequelize.sync().then(function() {
   /**
    * Listen on provided port, on all network interfaces.
    * 
    * Listen only when database connection is sucessfull
    */
-  app.listen(3000, function() {
+  app.listen(8080, function() {
     console.log('Express server listening on port 3000');
   });
-});
+//});
