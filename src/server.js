40d1607f3b48f3a0464d4ad8c7e91b4779af3255
Yuri Pons <Yuri.PONS@etu.uca.fr>
2018-11-30 09:57:35 +0100

    Modif port 8080

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-//const express = require('express')
-//const bodyParser = require('body-parser');
-//const morgan = require('morgan');
-//const app = express()
+const express = require('express')
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express()
 //const models = require('./models/index');
 
 console.log('Japprecie les fruits aux sirops')
